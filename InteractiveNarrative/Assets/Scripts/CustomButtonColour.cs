f4f7e62f0b540a980aaf0e36ddee6d58b9bd5b0f
Jacob Gibbins <jacob.gibbins2000@gmail.com>
2020-03-04 17:37:29 +0000

    Added cog
    Cog for options has been added
    Begun developing buttons transparency

---
+++ InteractiveNarrative/Assets/Scripts/CustomButtonColour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButtonColour : MonoBehaviour
+{
+
+    public Button button;
+
+    public void TurnWhite()
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = Color.white;
+        colors.highlightedColor = new Color32(225, 225, 225, 255);
+        button.colors = colors;
+    }
+
+
+     private void OnMouseEnter()
+    {
+        TurnWhite();
+    }
+}
---
+++ InteractiveNarrative/Assets/Scripts/EnableOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Cog;
+   
+   
+
+    private void OnMouseEnter()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
