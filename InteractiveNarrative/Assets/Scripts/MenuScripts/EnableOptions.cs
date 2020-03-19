4f941b163199b57cce8376fc830245a00cb7d7a1
Jacob Gibbins <jacob.gibbins2000@gmail.com>
2020-03-19 16:49:26 +0000

    intro scene tweaks + full options
    Added a fully working options menu,
    Some scene tweaks

---
+++ InteractiveNarrative/Assets/Scripts/MenuScripts/EnableOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject HighlightImage;
+    public SpriteRenderer HighlightSprite;
+    Animator anim;
+   
+
+    private void OnMouseEnter()
+    {
+        HighlightSprite.enabled = true;
+        anim.Play("Cog");
+    }
+
+
+    private void OnMouseExit()
+    {
+        HighlightSprite.enabled = false;
+    }
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
