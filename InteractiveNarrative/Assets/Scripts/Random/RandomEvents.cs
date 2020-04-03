e468ec8767a65cec66c14c7e210bae9c5023e054
Jacob Gibbins <jacob.gibbins2000@gmail.com>
2020-04-03 16:27:07 +0100

    Postit note header, Random events

---
+++ InteractiveNarrative/Assets/Scripts/Random/RandomEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class RandomEvents : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+     void Start()
+    {
+        PickRandomNumber(4);
+    }
+    
+
+    private void PickRandomNumber(int maxInt)
+    {
+        flowchart.SetIntegerVariable("randomNum", Random.Range(1, maxInt + 1));
+       
+    }
+    // Start is called before the first frame update
+    
+}
